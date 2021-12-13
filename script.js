2d1d5ce314c1f9f7282dbb526b1c1af99d9aa80f
Elias <eliasgalindo@gmail.com>
2021-12-13 13:09:09 -0300

    Requisito 7 feito - Adiciona background aos itens

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+// ### 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)
+const taskList = document.querySelector('#lista-tarefas');
+const inputText = document.getElementById('texto-tarefa');
 
 function createItem() {
-  const inputText = document.getElementById('texto-tarefa');
-  const taskList = document.getElementById('lista-tarefas');
   const liItem = document.createElement('li');
   liItem.innerText = inputText.value;
+  liItem.classList.add('item');
   taskList.appendChild(liItem);
   inputText.value = '';
 }
+function changeLiBgColor(event) {
+  const listItem = document.querySelector('.item');
+  listItem.classList.remove('item');
+  event.target.classList.add('listItem');
+}
 
 const buton = document.querySelector('#criar-tarefa');
+
 buton.addEventListener('click', createItem);
+
+taskList.addEventListener('click', changeLiBgColor);
